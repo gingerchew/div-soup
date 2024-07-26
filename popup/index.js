96ced1fe3026080caedd6f6cf2ccdfa660f309ce
ginger <26461046+gingerchew@users.noreply.github.com>
2024-07-26 14:02:03 -0400

    Converts the static rules and reasons properties to json
    this file is fetchable and should make it easier to add custom rules in the options

--- popup/index.js
+++ popup/index.js
@@ -1,55 +1,11 @@
 const notSelector = `*:not(div,script,style,link,noscript,template,slot,source,datalist,option,optgroup,track)`;
 
 class Soup {
-    static rules = {
-        'div': 0,
-        'div > div:not(:has( > div))': -1,
-        '[ping]': -0.1,
-        'div > div > div:not(:has( > div))': -5,
-        'div > div > div > div:not(:has( > div))': -10,
-        'div > div > div > div > div': -100,
-        '[href^="javascript:"]': -1000,
-        '[onclick]:not(button,a,input)': -1000,
-        [notSelector]: 1,
-    }
 
-    static reasons = {
-        'div': {
-            type: -1,
-            message: 'Used <code>&lt;div></code> $ times'
-        },
-        'div > div:not(:has( > div))': {
-            type: -1,
-            message: 'Used single level nested <code>&lt;div></code> elements $ times'
-        },
-        '[ping]': {
-            type: -1,
-            message: 'Used outdated tracking methods $ times'
-        },
-        'div > div > div:not(:has( > div))': {
-            type: -1,
-            message: 'Used double level nested <code>&lt;div></code> elements $ times'
-        },
-        'div > div > div > div:not(:has( > div))': {
-            type: -1,
-            message: 'Used triple level nested <code>&lt;div></code> elements $ times'
-        },
-        'div > div > div > div > div': {
-            type: -1,
-            message: 'Used quadruple level nested <code>&lt;div></code> elements $ times'
-        },
-        '[href^="javascript:"]': {
-            type: -1,
-            message: 'Used <code>&lt;a></code> tag instead of a button $ times'
-        },
-        '[onclick]:not(button,a,input)': {
-            type: -1,
-            message: 'Used <code>onclick</code> event handler on non-control elements $ times'
-        },
-        [notSelector]: {
-            type: 1,
-            message: 'Used elements that were not a <code>&lt;div></code> $ times'
-        },
+     static get rules() {
+        return new Promise(res => {
+            fetch('../data/rules.json').then(res => res.json()).then(_ => res(_));
+        });
     }
 
     static getClass(score) {
@@ -89,29 +45,31 @@ class Soup {
         return divs.length < non.length;
     }
 
-    static test(incomingHTML) {
+    static async test(incomingHTML) {
         this.score = 0;
         this.root.innerHTML = incomingHTML;
 
         const report = [];
-
-        for (const [selector, score] of Object.entries(this.rules)) {
+        const rules = await this.rules;
+        for (const { selector, type, message, weight } of rules) {
             const violators = this.root.querySelectorAll(selector);
-            console.log(violators);
+            
             if (violators.length === 0) continue;
-            violators.forEach(_ => this.score += score);
-            const { type, message } = this.reasons[selector];
+            violators.forEach(_ => this.score += weight);
+            // const { type, message } = this.reasons[selector];
             report.push([type, message.replace(/\$/, `<b>${violators.length}</b>`)]);
         }
 
         if (this.compareDivToNonDiv()) {
             report.push([1, `Used more non-<code>&lt;div></code> elements than <code>&lt;div></code> elements`])
         }
-        console.log(this.score);
+        
         if (this.score > 0) this.score = 0;
         requestAnimationFrame(() => {
+            console.log(this.score);
             this.output.textContent = this.score;
             this.output.classList.add(...this.getClass(this.score));
+
             for (const [type, message] of report) {
                 (type > 0 ? this.good : this.report).insertAdjacentHTML('beforeend', `<li>${message}</li>`);
             }
@@ -121,15 +79,23 @@ class Soup {
 
 window.onload = getCurrentDom;
 
+const formatURL = (str) => {
+    const url = new URL(str);
+    
+    return url.origin + url.pathname;
+}
+
 function getCurrentDom() {
-    chrome.tabs.query({ active: true, currentWindow: true }).then(([{ id }]) => {
+    chrome.tabs.query({ active: true, currentWindow: true }).then(([{ url, id }]) => {
+        document.getElementById('url').textContent = formatURL(url);
+
         return chrome.scripting.executeScript({
             target: { tabId: id },
             func: domToString
         })
     }).then(([{ result }]) => {
         Soup.test(result);
-        console.log('test');
+
     })
 }
 
